9eda651911ebdc8fa08ae488858d4041085b6707
alan-wu <alan.wu@auckland.ac.nz>
2023-04-12 12:38:57 +1200

    Commit so far.

--- vue.config.js
+++ vue.config.js
@@ -6,29 +6,17 @@ module.exports = {
       openAnalyzer: false
     }
   },
-    chainWebpack: config => {
-      // GraphQL Loader
-      config.module
-        .rule('shader')
-        .test(/\.(vs|fs)$/i)
-        .use('raw-loader')
-          .loader('raw-loader')
-          .end()
-      const fontsRule = config.module.rule('fonts')
-      fontsRule.uses.clear()
-      config.module
-        .rule('fonts')
-        .test(/\.(ttf|otf|eot|woff|woff2)$/)
-        .use('base64-inline-loader')
-        .loader('base64-inline-loader')
-        .tap(options => {
-          // modify the options...
-          return options
-        })
+  chainWebpack: config => {
+    // GraphQL Loader
+    config.module
+      .rule('shader')
+      .test(/\.(vs|fs)$/i)
+      .use('raw-loader')
+        .loader('raw-loader')
         .end()
-    },
-    devServer: {
-      disableHostCheck: true
+  },
+  devServer: {
+    allowedHosts: "all"
   },
   configureWebpack: config => {
     if(process.env.NODE_ENV === 'production') {
@@ -41,7 +29,7 @@ module.exports = {
     //Import variables into all stylesheets.
     loaderOptions: {
       sass: {
-        prependData: `@import '@/assets/styles';`
+        additionalData: `@import '@/assets/styles';`
       }
     }
   }
